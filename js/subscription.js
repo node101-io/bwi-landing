b7bd513b71453a40190ebdbf40a5435349d49a37
necipsagiro <sagiroglun22@itu.edu.tr>
2025-04-28 01:17:09 +0300

    fix subs

--- js/subscription.js
+++ js/subscription.js
@@ -11,18 +11,31 @@ window.addEventListener('load', () => {
         if (!event.target.closest('.subscription-wrapper')) {
             document.querySelector('.subscription-inner-wrapper').classList.remove('subscription-inner-wrapper-shifted');
         }
-
-        if (event.target.closest('.subscription-submit-button')) {
-            fetch("/api/subscribe", {
-                method: "POST",
-                body: JSON.stringify({ email: document.querySelector('.subscription-email-input').value }),
-            })
-                .then(res => res.json())
-                .then(res => {
-                    // TODO
-                    console.log(res);
-                })
-                .catch(console.error)
-        }
     });
+
+    document.addEventListener('submit', event => {
+        event.preventDefault();
+
+        fetch("https://node101.io/subscribe", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email: document.querySelector('.subscription-email-input').value,
+                type: 'buildersweekistanbul'
+            }),
+        })
+            .then(res => res.json())
+            .then(res => {
+                // TODO: update ui
+                if (!res || !('data' in res) || res.error) {
+                    console.log('failed');
+                    return;
+                }
+
+                console.log('success', res.data)
+            })
+            .catch(err => {
+                console.error('failed', err)
+            });
+    })
 });
